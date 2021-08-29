12e73bd2b9f568d4c14d68490d39c7fc10830a77
Robert Soriano <sorianorobertc@gmail.com>
2021-08-29 21:37:52 +0800

    add prepass option

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import type {
   EventHandler
 } from 'gqty'
 
+import { prepass } from 'gqty'
+
 import {
   computed,
   getCurrentInstance,
@@ -15,14 +17,14 @@ import {
   Ref
 } from 'vue-demi'
 
-type OnErrorHandler = (error: GQtyError) => void;
+type OnErrorHandler = (error: GQtyError) => void
 
 function hasEnabledStaleWhileRevalidate(
     staleWhileRevalidate: boolean | object | number | string | null
   ) {
     return typeof staleWhileRevalidate === 'boolean'
       ? staleWhileRevalidate
-      : true;
+      : true
 }
 
 function useSubscribeCacheChanges({
@@ -30,29 +32,29 @@ function useSubscribeCacheChanges({
   eventHandler,
   onChange,
 }: {
-  hookSelections: Ref<Set<Selection>>;
-  eventHandler: EventHandler;
-  onChange: () => void;
+  hookSelections: Ref<Set<Selection>>
+  eventHandler: EventHandler
+  onChange: () => void
 }) {
   let onChangeCalled = false
 
   const unsubscribeFetch = eventHandler.onFetchSubscribe((fetchPromise, promiseSelections) => {
-      if (onChangeCalled || !promiseSelections.some((selection) => hookSelections.value.has(selection))) {
-          return
-      }
+    if (onChangeCalled || !promiseSelections.some((selection) => hookSelections.value.has(selection))) {
+      return
+    }
 
-      onChangeCalled = true
-      fetchPromise.then(() => {
-          Promise.resolve(fetchPromise).then(onChange)
-      })
+    onChangeCalled = true
+    fetchPromise.then(() => {
+      Promise.resolve(fetchPromise).then(onChange)
+    })
   })
 
 const unsubscribeCache = eventHandler.onCacheChangeSubscribe(
   ({ selection }) => {
-          if (!onChangeCalled && hookSelections.value.has(selection)) {
-              onChangeCalled = true
-              Promise.resolve().then(onChange)
-          }
+    if (!onChangeCalled && hookSelections.value.has(selection)) {
+        onChangeCalled = true
+        Promise.resolve().then(onChange)
+    }
   }
 )
 
@@ -65,49 +67,72 @@ const unsubscribeCache = eventHandler.onCacheChangeSubscribe(
   })
 }
 
-interface Schema {
-  query: object;
-  mutation: object;
-  subscription: object;
+interface UseQueryPrepareHelpers<
+  GeneratedSchema extends {
+    query: object
+  }
+> {
+  readonly prepass: typeof prepass
+  readonly query: GeneratedSchema['query']
 }
 
-export function useQuery<GeneratedSchema extends Schema>(client: GQtyClient<GeneratedSchema>, {
+export function useQuery<GeneratedSchema extends {
+  query: object
+  mutation: object
+  subscription: object
+}>(client: GQtyClient<GeneratedSchema>, {
   onError,
+  prepare,
   staleWhileRevalidate = false
 }: {
   onError?: OnErrorHandler | undefined,
+  prepare?: (helpers: UseQueryPrepareHelpers<GeneratedSchema>) => void
   staleWhileRevalidate?: boolean
 }) {
   const fetchingPromise =  ref<Promise<void> | null>(null)
-  const query = shallowRef(client.query)
+  const clientQuery: GeneratedSchema['query'] = client.query
+  const query = shallowRef(clientQuery)
   const hookSelections = ref(new Set<Selection>())
-  const enabledStaleWhileRevalidate = hasEnabledStaleWhileRevalidate(staleWhileRevalidate);
-  const cacheRefetchSelections = enabledStaleWhileRevalidate ? new Set<Selection>() : null;
+  const enabledStaleWhileRevalidate = hasEnabledStaleWhileRevalidate(staleWhileRevalidate)
+  const cacheRefetchSelections = enabledStaleWhileRevalidate ? new Set<Selection>() : null
 
   const { eventHandler, scheduler, interceptorManager } = client
 
+  const prepareHelpers: UseQueryPrepareHelpers<GeneratedSchema> = {
+    prepass,
+    query: clientQuery,
+  }
+
   const { createInterceptor, globalInterceptor, removeInterceptor } = interceptorManager
-  const interceptor = createInterceptor();
+  const interceptor = createInterceptor()
+
+  if (prepare) {
+    try {
+      prepare(prepareHelpers)
+    } catch (err) {
+      throw err
+    }
+  }
 
   interceptor.selectionCacheRefetchListeners.add((selection) => {
-    if (cacheRefetchSelections) cacheRefetchSelections.add(selection);
+    if (cacheRefetchSelections) cacheRefetchSelections.add(selection)
 
-    hookSelections.value.add(selection);
-  });
+    hookSelections.value.add(selection)
+  })
 
   if (enabledStaleWhileRevalidate && cacheRefetchSelections?.size) {
     for (const selection of cacheRefetchSelections) {
-      globalInterceptor.addSelectionCacheRefetch(selection);
+      globalInterceptor.addSelectionCacheRefetch(selection)
     }
   }
 
   interceptor.selectionAddListeners.add((selection) => {
-    hookSelections.value.add(selection);
-  });
+    hookSelections.value.add(selection)
+  })
 
   interceptor.selectionCacheListeners.add((selection) => {
-    hookSelections.value.add(selection);
-  });
+    hookSelections.value.add(selection)
+  })
 
   const unsubscribeResolve = scheduler.subscribeResolve((promise, selection) => {
     if (fetchingPromise.value === null && hookSelections.value.has(selection)) {
